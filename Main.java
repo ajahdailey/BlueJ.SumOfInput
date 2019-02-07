634b6f6264caadf19f77ecfb2ac5ff780dc918a0
Ajah Dailey <ajahdailey@zipcoders-MBP-3.fios-router.home>
2019-02-06 22:32:29 -0500

    changes to lab

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
+     Scanner scanner = new Scanner(System.in);
+     
+     System.out.println("Provide a posotive integer");
+     
+        int input = scanner.nextInt();
+        int sum = 0;
+     for( int i = 0; i <= input ; i++ ) {
+         sum = sum + i;
+     
     }
+    System.out.println(sum);
+}
 }
